efd91f073de7ce9d927e99ed3ce5b6787f326a86
wojciech <wojciech.adaszynski@gmail.com>
2016-04-20 20:10:14 +0200

    deps and file deletion

--- deps.sh
+++ deps.sh
@@ -1,11 +1,11 @@
-# #!/bin/bash
-#
-# set -o errexit
-#
-# apt-get -q update
-# apt-get install -y cowsay
-# # installing node enviroment
-# apt-get install -y nodejs
-# apt-get install -y npm
-#
-# /usr/games/cowsay "Install dependencies using a script like this!"
+#!/bin/bash
+
+set -o errexit
+
+apt-get -q update
+apt-get install -y cowsay
+# installing node enviroment
+apt-get install -y nodejs
+apt-get install -y npm
+
+/usr/games/cowsay "Install dependencies using a script like this!"
